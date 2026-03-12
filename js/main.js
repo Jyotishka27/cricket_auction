45bb098e24e67af2e3a5715ba1fc4c4faf10ff69
Jyotishka Mukhopadhyay <76933417+Jyotishka27@users.noreply.github.com>
2026-03-12 21:57:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 (async function initAuction() {
+
   await loadAuctionData();
 
   const restored = restoreAutoSavedState();
 
+  if (restored) {
+    console.log("Auction state restored from autosave.");
+  }
+
   wireEvents();
   renderAll();
+
+  // Auto save every 5 seconds
+  setInterval(autoSaveState, 5000);
+
 })();
