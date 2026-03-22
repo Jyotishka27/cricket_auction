890b532473ecfaab3838299d8ba35675e08edb09
Jyotishka Mukhopadhyay <76933417+Jyotishka27@users.noreply.github.com>
2026-03-22 19:19:22 +0530

    Update state.js

--- js/state.js
+++ js/state.js
@@ -5,6 +5,16 @@ const state = {
   current: null,
   teams: [],
   sales: [],
+  rules: {
+    minPlayersPerTeam: 4,
+    maxPlayersPerTeam: 5,
+    pools: {
+      X: { mandatory: true, min: 1, max: 2 },
+      P: { mandatory: true, min: 1, max: 2 },
+      A: { mandatory: true, min: 1, max: 2 },
+      B: { mandatory: true, min: 1, max: 2 }
+    }
+  },
   timer: { handle: null, left: 0, running: false },
   ui: {
     activeMainTab: 'auction',
